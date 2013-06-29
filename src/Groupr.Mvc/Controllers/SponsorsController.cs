e2c8bdd4f49fd8c45ca4ce45bdcd76f16fa4d189
Philip Proplesch <philip@proplesch.de>
2013-06-29 18:37:08 +0200

    Added static sponsor images.

---
+++ src/Groupr.Mvc/Controllers/SponsorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Groupr.Mvc.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
