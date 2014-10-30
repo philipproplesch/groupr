3944a05b0d291155116e6bc33f200ec7298cf6ee
Philip Proplesch <philip@proplesch.de>
2014-10-30 20:37:00 +0100

    B: Disabled model validation for ChangePassword

--- src/Groupr.Mvc/ViewModels/ChangePasswordViewModel.cs
+++ src/Groupr.Mvc/ViewModels/ChangePasswordViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Groupr.Mvc.ViewModels
 {
+    [ValidateInput(false)]
     public class ChangePasswordViewModel
     {
         public string Token { get; set; }
 
         [Required]
+        [MaxLength(128)]
         public string NewPassword { get; set; }
 
         [Required]
-        [Compare("NewPassword")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword")]
+
+        [MaxLength(128)]
         public string ConfirmNewPassword { get; set; }
     }
 }
